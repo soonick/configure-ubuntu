51356a59cf6dd2d37299b7045424e4caa44a024b
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2020-04-29 22:17:14 +1000

    Use the newest supported version of docker. Fixes #4

--- do.sh
+++ do.sh
@@ -35,7 +35,7 @@ apt-get install -y \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   disco \
+   eoan \
    stable"
 apt-get update -y
 apt-get install -y docker-ce docker-ce-cli containerd.io
