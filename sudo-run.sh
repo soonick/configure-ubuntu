a999c2e2fa5c5faefe3c878d8e5d60e1d5675c3e
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2022-12-31 15:11:36 -0300

    Fix docker compose installation

--- sudo-run.sh
+++ sudo-run.sh
@@ -37,7 +37,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 
 myself=`logname`
 
