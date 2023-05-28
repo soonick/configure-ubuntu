622d799d8eca4b830aaa65a3da75f37d06653c6d
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2023-05-28 15:41:21 -0300

    Make sudo script idempotent

--- sudo-run.sh
+++ sudo-run.sh
@@ -32,21 +32,43 @@ apt-get install -y \
   gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 gnome-system-monitor
 
 # Install docker and docker compose
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+if command -v docker &> /dev/null
+then
+  echo "Docker is already installed"
+else
+  echo "Installing docker and docker compose"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+fi
 
 myself=`logname`
+mkdir -p /home/$myself/bin/
 
 # Install kubectl
-curl -L -o /home/$myself/bin/kubectl \
-  "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-install -o root -g root -m 0755 /home/$myself/bin/kubectl /usr/local/bin/kubectl
+if command -v kubectl &> /dev/null
+then
+  echo "Kubectl is already installed"
+else
+  echo "Installing kubectl"
+  curl -L -o /home/$myself/bin/kubectl \
+    "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+  install -o root -g root -m 0755 /home/$myself/bin/kubectl /usr/local/bin/kubectl
+fi
 
 # Install Chrome
-wget -O /home/$myself/bin/chrome.deb \
-  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install /home/$myself/bin/chrome.deb
+if command -v google-chrome &> /dev/null
+then
+  echo "Chrome is already installed"
+else
+  echo "Installing chrome"
+  wget -O /home/$myself/bin/chrome.deb \
+    https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install /home/$myself/bin/chrome.deb
+fi
+
+chown -R $myself /home/$myself/bin
+chgrp -R $myself /home/$myself/bin
