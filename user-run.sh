7e4de6a8143d603ca6f0d04b7d2687ba42fb461c
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2024-05-10 13:44:50 +0200

    Use dark theme

--- user-run.sh
+++ user-run.sh
@@ -74,6 +74,9 @@ gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
 # When switching workspace, switch all monitors
 gsettings set org.gnome.mutter workspaces-only-on-primary false
 
+# Use dark theme
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
 # Configure terminal
 GNOME_TERMINAL_PROFILE=`gsettings get org.gnome.Terminal.ProfilesList default | awk -F \' '{print $2}'`
 gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$GNOME_TERMINAL_PROFILE/ font 'Monospace 10'
